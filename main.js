3c8a622a8df26b355680222a1ee9214dcaa89597
vranceanum <markdevstudio@gmail.com>
2018-01-20 18:59:45 +0200

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+// Date Picker
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
+//   Main Content
+function openTab(evt , tabChange) {
+    let i;
+    let tabcontent = document.getElementsByClassName('tabcontent');
+    for ( let i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display= 'none';
+    }
+    document.getElementById(tabChange).style.display = "block";
+  
+}
+document.getElementById("defaultOpen").click();
