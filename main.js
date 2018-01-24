16503f7ac6a41d5a7a321fdca57896140fb9e08f
VranceanuM <markdevstudio@gmail.com>
2018-01-24 15:01:06 +0200

    final UI

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // Date Picker
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    selectYears: 95, // Creates a dropdown of 15 years to control year,
     today: 'Today',
     clear: 'Clear',
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
-//   Main Content
+// FormJS
+
+
+
+
+
+
+
+
+//   Main Content Change
 function openTab(evt , tabChange) {
     let i;
     let tabcontent = document.getElementsByClassName('tabcontent');
